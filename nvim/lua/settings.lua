94145f32d7c7af333b17bc02423dd2f82727e53f
Fraz Ali <frazaliqureshi@yahoo.com>
2021-11-20 20:52:34 +0100

    nvim: use lua to laod colorscheme

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -3,5 +3,6 @@ vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
 vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
 
 -- Set colorscheme
-vim.cmd([[ colorscheme nightfox ]])
+-- vim.cmd([[ colorscheme nightfox ]])
+require('nightfox').load()
 
