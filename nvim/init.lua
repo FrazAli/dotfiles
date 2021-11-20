bc429dfca60e68bbd85e688904c02ff912f83ff3
Fraz Ali <frazaliqureshi@yahoo.com>
2021-11-20 21:40:54 +0100

    nvim: added nihgtfox config, reorganized settings into separate files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,12 @@
-require('settings')
-require('keymaps')
-
 -- Plugins
 require('lualine').setup()
 require('nvim-tree').setup()
 require('nvim-treesitter').setup()
+
+--keymaps
+require('keymaps')
+
+-- Settings
+require('settings/general')
+require('settings/nightfox')
+
--- nvim/lua/settings/general.lua
+++ nvim/lua/settings/general.lua
@@ -2,7 +2,3 @@
 vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
 vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
 
--- Set colorscheme
--- vim.cmd([[ colorscheme nightfox ]])
-require('nightfox').load()
-
