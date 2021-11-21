454344786910b46cb28eeba08337aee98104d0a7
Fraz Ali <frazaliqureshi@yahoo.com>
2021-11-21 20:16:03 +0100

    nvim: re-organized config files and folders

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,12 @@
+-- Themes
+require('themes/nightfox')
+
+-- Settings
+require('keymaps')
+require('settings')
+
 -- Plugins
 require('lualine').setup()
 require('nvim-tree').setup()
 require('nvim-treesitter').setup()
 require('nvim-web-devicons').setup()
-
---keymaps
-require('keymaps')
-
--- Settings
-require('settings/general')
-require('settings/nightfox')
-
