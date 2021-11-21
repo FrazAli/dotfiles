e54b7c10fc0781457c53b21394619be82ce7b7ad
Fraz Ali <frazaliqureshi@yahoo.com>
2021-11-21 20:42:26 +0100

    nvim: added Telescope plugin with its dependencies

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,3 +10,7 @@ require('lualine').setup()
 require('nvim-tree').setup()
 require('nvim-treesitter').setup()
 require('nvim-web-devicons').setup()
+require('plenary')
+
+-- Plugins with config.
+require('plugins/telescope')
