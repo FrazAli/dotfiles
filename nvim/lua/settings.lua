f2723fd634a21691c59a1b25c3d8114201303714
Fraz Ali <fraz.ali@atlasantibodies.com>
2021-12-04 16:27:59 +0100

    nvim: added line number settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,3 +2,9 @@
 vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
 vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
 
+-- Line numers
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.numberwidth = 4
+-- vim.wo.signcolumn = number
+
