dce62675ed2a5fba1a042814944849d534143987
Fraz Ali <frazaliqureshi@yahoo.com>
2021-12-05 20:16:59 +0100

    nvim: added keymaps for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -38,3 +38,8 @@ telescope.setup({
 })
 
 telescope.load_extension('fzf')
+
+-- keymaps
+vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
