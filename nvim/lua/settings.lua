6ea5d2a67545131e3e2e3156ff33eb0f7eaeb1bd
Fraz Ali <frazaliqureshi@yahoo.com>
2021-12-05 19:28:28 +0100

    nvim: updated settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,11 +2,35 @@
 vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
 vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
 
+-- General
+vim.o.scrolloff = 3           -- Number of lines offset for scrolling text
+vim.o.errorbells = false      -- System bell on error
+vim.o.termguicolors = true    -- 24-bit RGB color support
+vim.o.showmode = false        -- Show mode in the command line
+vim.o.showtabline = 2         -- When to show buffer tab line, 2 = always
+vim.o.splitbelow = true       -- Default vertical split
+vim.o.splitright = true       -- Default horizontal split
+vim.o.swapfile = true         -- Enable swap file
+vim.o.wrap = false            -- Wrap text
+vim.o.colorcolumn = '120'     -- Column number to mark for recommended max. line length
+vim.o.cursorline = true       -- Highlight cursol line
+
+
 -- Line numers
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.numberwidth = 4
--- vim.wo.signcolumn = number
+vim.wo.signcolumn = 'number'  -- Column to show signs e.g. for lsp and diagnostics
+
+-- Tab and indentation
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+-- Buffer search
+vim.o.incsearch = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
 
 -- Enable completion pop ups
 vim.o.completeopt="menu,menuone,preview,noinsert,noselect"
