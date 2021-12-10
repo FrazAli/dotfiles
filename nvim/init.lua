60e341239dd0c5e0b83482ba38763951465178d6
Fraz Ali <fraz.ali@atlasantibodies.com>
2021-12-10 16:53:38 +0100

    nvim: added config for lualine plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,11 +6,11 @@ require('keymaps')
 require('settings')
 
 -- Plugins with default or no config.
-require('lualine').setup()
 require('nvim-web-devicons').setup()
 require('plenary')
 
 -- Config. for plugins
+require('plugins/lualine')
 require('plugins/telescope')
 require('plugins/nvim-tree')
 require('plugins/nvim-treesitter')
