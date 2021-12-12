7ba2b513aabd4f19a3503716ab420d24f173eb70
Fraz Ali <frazaliqureshi@yahoo.com>
2021-12-12 10:43:45 +0100

    nvim: disabled annoying javascript hints from tsserver

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -104,8 +104,14 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['tsserver'].setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+  init_options = {
+        preferences = {
+            disableSuggestions = true
+        }
+    }
 }
+
 require('lspconfig')['pyright'].setup {
   capabilities = capabilities
 }
