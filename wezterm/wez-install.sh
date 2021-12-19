46c8525355c80a1bc2a6b13d76de4785a224ace8
Fraz Ali <frazaliqureshi@yahoo.com>
2021-12-19 16:56:17 +0100

    wezterm: Added script to install wezterm on debian

---
+++ wezterm/wez-install.sh
@@ -0,0 +1,3 @@
+!#/usr/bin/env bash
+curl -LO https://github.com/wez/wezterm/releases/download/20211205-192649-672c1cc1/wezterm-20211205-192649-672c1cc1.Ubuntu20.04.deb
+sudo apt install -y ./wezterm-20211205-192649-672c1cc1.Ubuntu20.04.deb
