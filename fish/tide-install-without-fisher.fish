d47a74da53e39dc13c25752c56cfda98a29c95a3
Fraz Ali <frazaliqureshi@gmail.com>
2022-08-23 19:49:53 +0200

    fish: updated script to install tide plugin

--- fish/tide-install-without-fisher.fish
+++ fish/tide-install-without-fisher.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/env fish
 
 set -l _tide_tmp_dir (command mktemp -d)
-curl https://codeload.github.com/ilancosman/tide/tar.gz/HEAD | tar -xzC $_tide_tmp_dir
-command cp -R $_tide_tmp_dir/tide-HEAD/{completions,conf.d,functions} $__fish_config_dir
-exec fish --init-command "set -g fish_greeting; emit _tide_init_install"
-
+curl https://codeload.github.com/ilancosman/tide/tar.gz/v5 | tar -xzC $_tide_tmp_dir
+command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
+fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
