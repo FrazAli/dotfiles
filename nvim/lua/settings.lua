f40ca36df519bb3dc24484ff3ee43078a2a1e579
Fraz Ali <frazaliqureshi@yahoo.com>
2022-11-02 16:34:56 +0100

    nvim: updated comment

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -30,7 +30,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
--- Buffer search
+-- Buffer search and highlight
 vim.o.incsearch = true
 vim.o.hlsearch = false
 vim.o.ignorecase = true
