124e632dad1c1b53b9b5ba90405a55762a46a0d4
Fraz Ali <frazaliqureshi@gmail.com>
2022-11-05 13:41:17 +0100

    wezterm: disable audio bell

--- wezterm/wezterm.windows.lua
+++ wezterm/wezterm.windows.lua
@@ -34,6 +34,7 @@ return {
   },
   enable_tab_bar = false,
   window_background_opacity = 0.7,
+  audible_bell = "Disabled",
 
   -- This changes the default program from cmd.exe to wsl.exe
   -- Note: In order to change the distribution for wsl, set default
