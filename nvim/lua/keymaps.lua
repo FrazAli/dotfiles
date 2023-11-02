7b7482c7bdc80160a8f4e1435f0d8f260d56e3a0
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-03 00:18:01 +0100

    nvim: added key bindings for LSP completions

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,6 +7,14 @@ local function keymap(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+--TODO: check if keymap funciton implentaion can be merged with this.
+local nmap = function (keys, func, desc)
+  if desc then
+    desc = 'LSP: ' .. desc
+  end
+
+  vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+end
 
 keymap('', '<Space>', '<Nop>')
 vim.g.mapleader = ' '
@@ -21,3 +29,8 @@ keymap('n', '<Leader>t', ":sp<CR> :term<CR> :resize 20N<CR> i")
 
 -- Open diagnostic hint on current line in a floating window
 keymap('n', '<Leader>h', ':lua vim.diagnostic.open_float()<CR>')
+
+--LSP and code completions
+nmap('gd', vim.lsp.buf.definition, '[G]oto [D]definition')
+nmap('gi', vim.lsp.buf.definition, '[G]oto [I]mplementation')
+nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
