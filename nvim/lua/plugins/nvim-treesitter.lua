f2eda311cf1568c0eecc628b12210eb643e7727c
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-02 22:42:09 +0100

    nvim: updated treesitter to indlude web dev languages

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,18 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust"},
+  ensure_installed = {
+    "c",
+    "css",
+    "html",
+    "javascript",
+    "lua",
+    "python",
+    "query",
+    "rust",
+    "typescript",
+    "vim",
+    "vimdoc"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
