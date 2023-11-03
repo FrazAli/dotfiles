13d000e3a2fcf902c5ab02a9cb67a22284a29d56
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-03 12:13:15 +0100

    nvim: added settings for undercurl

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,6 +2,10 @@
 vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
 vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
 
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
 -- Set theme
 vim.cmd("colorscheme nightfox")
 
