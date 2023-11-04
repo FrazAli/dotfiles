8f440c3a3766d95aff5cfae91bfa5ed5bceb7cb3
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-04 14:42:54 +0100

    nvim: commented plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,27 @@
 -- Themes
 -- require('themes/material')
-require('themes/nightfox')
+require('themes/nightfox')            -- Dark theme
 
 -- Settings
-require('keymaps')
-require('settings')
+require('keymaps')                    -- General key maps
+require('settings')                   -- Editor settings
 
 -- Plugins with default or no config.
-require('mason').setup()
-require('nvim-ts-autotag').setup()
-require('nvim-autopairs').setup()
-require('nvim-web-devicons').setup()
-require('lspsaga').setup()
-require('plenary')
+require('mason').setup()              -- Manages LSP servers and tools
+require('nvim-ts-autotag').setup()    -- Auto-tags HTML using 'nvim-treesitter'
+require('nvim-autopairs').setup()     -- Auto-pairs braces / brackets and more
+require('nvim-web-devicons').setup()  -- Installs Nerd font symbols and icons
+require('lspsaga').setup()            -- LSP completion front-end
+require('plenary')                    -- Lua library needed by other plugins e.g. telescope
 
 -- Plugins with custom config.
-require('plugins/colorizer')
-require('plugins/lualine')
-require('plugins/mason-lspconfig')
-require('plugins/nvim-tree')
-require('plugins/nvim-treesitter')
-require('plugins/prettier')
-require('plugins/telescope')
+require('plugins/colorizer')          -- highlights color codes with the color
+require('plugins/lualine')            -- Status line with symbols and theme
+require('plugins/mason-lspconfig')    -- integrates 'mason' with 'nvim-lspconfig'
+require('plugins/nvim-tree')          -- Side menu file browser
+require('plugins/nvim-treesitter')    -- Tree-sitter support for many languages
+require('plugins/prettier')           -- Helps prettify code formatting
+require('plugins/telescope')          -- Fuzzy finder with preview
 
 -- Code Completion
-require('plugins/nvim-cmp')
+require('plugins/nvim-cmp')           -- Completion plugin
