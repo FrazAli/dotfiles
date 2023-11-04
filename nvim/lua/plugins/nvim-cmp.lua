128354afbc7d03157d318ad4ce5234fb8b361551
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-04 12:27:43 +0100

    Revert "nvim: removed lspking symbols."
    This reverts commit e42cf7d5f3b126d9ff5c052d22c15f733d3e2f82.
    
    Reverted to try different icons

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,31 @@
+local kind_icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = ""
+}
+
 local cmp = require'cmp'
 
 cmp.setup({
@@ -37,6 +65,8 @@ cmp.setup({
   }),
   formatting = {
     format = function(entry, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- Source
       vim_item.menu = ({
         buffer = "﬘",
