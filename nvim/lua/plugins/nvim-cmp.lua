f14af7544d250bd208048a8e89b622567cc2f061
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-05 13:06:12 +0100

    nvim: auto-complete selections using 'ENTER' key

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -50,7 +50,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = cmp.config.sources({
     { name = 'luasnip' },                 -- For luasnip users.
