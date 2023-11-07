dcd6e4b3af41bcda0177ec1a0ff96e6388c0a68c
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-07 11:23:52 +0100

    nvim: added plugin 'gitsigns'

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ require('plenary')                    -- Lua library needed by other plugins e.g
 
 -- Plugins with custom config.
 require('plugins/colorizer')          -- highlights color codes with the color
+require('plugins/gitsigns')           -- Git integration
 require('plugins/lualine')            -- Status line with symbols and theme
 require('plugins/mason')              -- Manages LSP servers and tools
 require('plugins/mason-lspconfig')    -- integrates 'mason' with 'nvim-lspconfig'
