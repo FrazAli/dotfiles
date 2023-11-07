0b9d596c21ac066e38304f44b7c10b4235715427
Fraz Ali <frazaliqureshi@gmail.com>
2023-11-07 01:25:10 +0100

    nvim: added 'nerog' plugin for notes and journal

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,19 @@
+require('neorg').setup({
+  load = {
+    ['core.defaults'] = {},
+    -- ['core.norg.concealer'] = {},
+    -- ['core.norg.completion'] = {
+    --   config = {
+    --     engine = 'nvim-cmp',
+    --   },
+    -- },
+    -- ['core.dirman'] = {
+    --   config = {
+    --     workspaces = {
+    --       notes = '~/notes',
+    --     },
+    --     default_workspace = 'notes',
+    --   }
+    -- }
+  }
+})
