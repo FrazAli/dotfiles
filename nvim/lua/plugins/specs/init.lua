283bb07899098b833cc8108143bd65b93afe4a00
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-11 21:03:16 +0100

    nvim: lazy load plugin nvim-autopairs

--- nvim/lua/plugins/specs/init.lua
+++ nvim/lua/plugins/specs/init.lua
@@ -1,7 +1,10 @@
 -- Plugins with default or no config.
 return {
-  "windwp/nvim-autopairs", -- Auto-pairs braces / brackets and more
-  -- "windwp/nvim-ts-autotag", -- Auto-tags HTML using 'nvim-treesitter'
+  {
+    "windwp/nvim-autopairs", -- Auto-pairs braces / brackets and more
+    event = "InsertEnter",
+    opts = {},  -- invokes setup({}) with default config.
+  },
   "nvim-lua/plenary.nvim", -- Lua library needed by other plugins e.g. telescope
 }
 
