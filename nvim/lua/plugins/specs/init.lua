3e9d5806c04ade9ef3af221395344645f14b3fc1
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-11 19:19:56 +0100

    nvim: plugins specs without config

---
+++ nvim/lua/plugins/specs/init.lua
@@ -0,0 +1,7 @@
+-- Plugins with default or no config.
+return {
+  "windwp/nvim-autopairs", -- Auto-pairs braces / brackets and more
+  -- "windwp/nvim-ts-autotag", -- Auto-tags HTML using 'nvim-treesitter'
+  "nvim-lua/plenary.nvim", -- Lua library needed by other plugins e.g. telescope
+}
+
