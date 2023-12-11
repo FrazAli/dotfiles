f4806e6a720775fe19ef8c98a62f7f76d74b3790
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-11 21:12:31 +0100

    moved plugin to its own spec file

--- nvim/lua/plugins/specs/init.lua
+++ nvim/lua/plugins/specs/init.lua
@@ -1,10 +1,5 @@
 -- Plugins with default or no config.
 return {
-  {
-    "windwp/nvim-autopairs", -- Auto-pairs braces / brackets and more
-    event = "InsertEnter",
-    opts = {},  -- invokes setup({}) with default config.
-  },
   "nvim-lua/plenary.nvim", -- Lua library needed by other plugins e.g. telescope
 }
 
---
+++ nvim/lua/plugins/specs/nvim-autopairs.lua
@@ -0,0 +1,6 @@
+return {
+  "windwp/nvim-autopairs", -- Auto-pairs braces / brackets and more
+  event = "InsertEnter",
+  opts = {},  -- invokes setup({}) with default config.
+}
+
