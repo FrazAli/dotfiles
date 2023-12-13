01de2a78bbd329c407d7cdd65b4c029676d18f74
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-13 10:49:58 +0100

    added formatting plugin

---
+++ nvim/lua/plugins/formatting/conform.lua
@@ -0,0 +1,39 @@
+return {
+  "stevearc/conform.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        css = { "prettier" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        python = { "isort", "black" }, -- conform runs multiple formatters sequentially
+        svelte = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        yaml = { "prettier" },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        async = false, -- setting this true means the timeout has no effect
+        timeout_ms = 500, -- formatting timeout for sync formatting
+        lsp_fallback = true, -- fallback to LSP if there is no Formatter
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format({
+        async = false,
+        timeout_ms = 1000,
+        lsp_fallback = true,
+    })
+    end, { desc = "[F]ormat file or range (in visual mode)" })
+  end,
+}
