f4811b67d5e34ee64c1e694a99e5595d0d88c232
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-14 15:36:41 +0100

    added spec for plugin image

---
+++ nvim/lua/plugins/image.lua
@@ -0,0 +1,19 @@
+return {
+	"samodostal/image.nvim",
+	event = { "BufReadPre" },
+	config = function()
+		require("image").setup({
+			render = {
+				min_padding = 5,
+				show_label = true,
+				show_image_dimensions = true,
+				use_dither = true,
+				foreground_color = false,
+				background_color = false,
+			},
+			events = {
+				update_on_nvim_resize = true,
+			},
+		})
+	end,
+}
