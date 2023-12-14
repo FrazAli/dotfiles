a612bcfa64643a7f557e00a206871109175afc1e
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-14 22:51:57 +0100

    nvim: configure Nerog concealer

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -6,7 +6,23 @@ return {
 		require("neorg").setup({
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.concealer"] = {
+					config = {
+						icon_preset = "basic",
+						icons = {
+							code_block = {
+								conceal = true,
+								content_only = false, -- include @code and @end lines in highlight
+								width = "content",
+								padding = {
+									left = 2,
+									right = 2,
+								},
+								highlight = "CursorLine", -- set code block highlight to CursorLine
+							},
+						},
+					},
+				}, -- Adds pretty icons to your documents
 				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
