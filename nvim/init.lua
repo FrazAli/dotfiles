c3ed9052b6484859e113688bf3727fc289ae8ffd
Fraz Ali <frazaliqureshi@gmail.com>
2023-12-14 16:32:37 +0100

    cleanup of unused dead code

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,29 +4,3 @@ require("settings") -- Editor settings
 
 -- Plugin Manager
 require("plugins/manager/lazy")
--- -- Themes
--- -- require('themes/material')
--- -- require('themes/nightfox')            -- Dark theme
--- require("themes/catppuccin") -- Pastel theme
--- -- require('themes/kanagawa')            -- Pastel theme based on painting by Katsushika Hokusai
---
---
---
--- -- Plugins with custom config.
--- require("plugins/color-picker") -- Color picker
--- require("plugins/conform") -- Auto-format on save
--- require("plugins/gitsigns") -- Git integration
--- require("plugins/hologram") -- Markdown image preview
--- require("plugins/image") -- Ascii image preview
--- require("plugins/lualine") -- Status line with symbols and theme
--- require("plugins/mason") -- Manages LSP servers and tools
--- require("plugins/mason-lspconfig") -- integrates 'mason' with 'nvim-lspconfig'
--- require("plugins/neorg") -- Neorg for note taking
--- require("plugins/nvim-highlight-colors") -- Highlights color codes
--- require("plugins/nvim-tree") -- Side menu file browser
--- require("plugins/nvim-treesitter") -- Tree-sitter support for many languages
--- require("plugins/prettier") -- Helps prettify code formatting
--- require("plugins/telescope") -- Fuzzy finder with preview
---
--- -- Code Completion
--- require("plugins/nvim-cmp") -- Completion plugin
