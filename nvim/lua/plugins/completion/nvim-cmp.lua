677ca8ad0c562103a643dcfb59e35fb6eb56351a
Fraz Ali <frazaliqureshi@gmail.com>
2024-06-16 19:35:49 +0200

    nvim: added ghost_text option for nvim-cmp completions

--- nvim/lua/plugins/completion/nvim-cmp.lua
+++ nvim/lua/plugins/completion/nvim-cmp.lua
@@ -25,6 +25,7 @@ return {
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
+			ghost_text = { hl_group = "CmpGhostText" },
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
