55c7cccbe73c54127f6a4865bb96121e99dead8c
Fraz Ali <frazaliqureshi@gmail.com>
2024-07-30 16:10:21 +0200

    nvim: added luarocks dependency

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,9 @@
 -- Plugins with default or no config.
 return {
 	"nvim-lua/plenary.nvim", -- Lua library needed by other plugins e.g. telescope
+	{
+		"vhyrro/luarocks.nvim", -- Luarocks is needed by Mason and other plugins
+		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+		config = true,
+	},
 }
