b4870ba1d6ae3e1906f7ecdef896115ea25e8447
Fraz Ali <frazaliqureshi@gmail.com>
2024-12-20 19:40:39 +0100

    feat(wezterm): configure window opacity and mouse click through

--- wezterm/wezterm.macOS.lua
+++ wezterm/wezterm.macOS.lua
@@ -2,20 +2,35 @@
 -- Copy and rename this file into user's home directory, e.g.
 --      $HOME/.wezterm.lua
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  -- Font properties
-  font = wezterm.font("Hack Nerd Font Mono", {
-    weight="Bold",
-    stretch="Normal",
-    style="Normal"
-  }),
+	-- Font properties
+	font = wezterm.font("Hack Nerd Font Mono", {
+		weight = "Bold",
+		stretch = "Normal",
+		style = "Normal",
+	}),
 
-  font_size = 16.0,
+	font_size = 16.0,
 
-  -- Wondow properties
-  enable_tab_bar = false,
-  window_background_opacity = 0.85,
+	-- Wondow properties
+	enable_tab_bar = false,
+
+	-- Color scheme
+	color_scheme = "Catppuccin Mocha",
+
+	-- Background opacity and blur
+	window_background_opacity = 0.6,
+	macos_window_background_blur = 30,
+
+	-- Click to open url in the browser
+	mouse_bindings = {
+		-- Ctrl-click will open the link under the mouse cursor
+		{
+			event = { Up = { streak = 1, button = "Left" } },
+			mods = "CMD",
+			action = wezterm.action.OpenLinkAtMouseCursor,
+		},
+	},
 }
-
