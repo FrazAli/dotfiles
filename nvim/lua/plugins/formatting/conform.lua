fd4e0cbbbd86a409c61c3c07bf28d3224722c140
Fraz Ali <fraz.ali@helicon.ai>
2025-02-06 07:32:01 +0100

    fix(nvim): corrected python format command

--- nvim/lua/plugins/formatting/conform.lua
+++ nvim/lua/plugins/formatting/conform.lua
@@ -14,7 +14,7 @@ return {
 				json = { "prettier" },
 				lua = { "stylua" },
 				markdown = { "prettier" },
-				python = { "ruff" }, -- conform runs multiple formatters sequentially
+				python = { "ruff format" }, -- conform runs multiple formatters sequentially
 				svelte = { "prettier" },
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
