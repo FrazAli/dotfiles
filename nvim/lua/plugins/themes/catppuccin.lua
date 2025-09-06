86761fc4efded777cac1149b62aca337ce22f795
Fraz Ali <frazaliqureshi@gmail.com>
2025-09-06 23:55:09 +0200

    feat(nvim): updated current line number highlight

--- nvim/lua/plugins/themes/catppuccin.lua
+++ nvim/lua/plugins/themes/catppuccin.lua
@@ -40,7 +40,11 @@ return {
 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
 			color_overrides = {},
-			custom_highlights = {},
+			custom_highlights = function(colors)
+				return {
+					CursorLineNr = { fg = colors.peach, bold = true },
+				}
+			end,
 			default_integrations = true,
 			auto_integrations = false,
 			integrations = {
