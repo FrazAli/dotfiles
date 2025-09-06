c4b6378e499a17eb194a80bd7a9c3c3ce98b95e4
Fraz Ali <frazaliqureshi@gmail.com>
2025-09-06 23:05:48 +0200

    chore(nvim): updated theme config to current default except transparency

--- nvim/lua/plugins/themes/catppuccin.lua
+++ nvim/lua/plugins/themes/catppuccin.lua
@@ -4,12 +4,16 @@ return {
 	priority = 1000, -- colorscheme loads before eveything else
 	config = function()
 		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			flavour = "mocha", -- latte, frappe, macchiato, mocha, auto
 			background = { -- :h background
 				light = "latte",
 				dark = "mocha",
 			},
 			transparent_background = true, -- disables setting the background color.
+			float = {
+				transparent = true, -- enable transparent floating windows
+				solid = false, -- use solid styling for floating windows, see |winborder|
+			},
 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
@@ -33,15 +37,12 @@ return {
 				properties = {},
 				types = {},
 				operators = {},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
-			color_overrides = {
-				mocha = {
-					base = "#484848", -- slightly less dark than default
-				},
-			},
-			custom_highlights = {
-				LineNr = { fg = "#797989" },
-			},
+			color_overrides = {},
+			custom_highlights = {},
+			default_integrations = true,
+			auto_integrations = false,
 			integrations = {
 				cmp = true,
 				gitsigns = true,
@@ -56,7 +57,7 @@ return {
 			},
 		})
 
-		-- set the color-scheme
-		vim.cmd([[ colorscheme catppuccin-mocha ]])
+		-- setup must be called before loading
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
