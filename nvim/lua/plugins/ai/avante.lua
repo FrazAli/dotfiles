28f9fd000ed61d2c70bc625cad5584342aca147b
Fraz Ali <frazaliqureshi@gmail.com>
2025-09-20 00:20:08 +0200

    fix(nvim): remove unused render-markdown plugin
    its highlight groups conflict with markview

--- nvim/lua/plugins/ai/avante.lua
+++ nvim/lua/plugins/ai/avante.lua
@@ -39,14 +39,6 @@ return {
 				},
 			},
 		},
-		{
-			-- Make sure to setup it properly if you have lazy=true
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = {
-				file_types = { "markdown", "Avante" },
-			},
-			ft = { "markdown", "Avante" },
-		},
 	},
 }
 
