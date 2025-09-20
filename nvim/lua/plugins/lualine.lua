a43c77b537a0668b1c372a9329f9bbdb8d26a1cf
Fraz Ali <frazaliqureshi@gmail.com>
2025-09-20 15:48:50 +0200

    nvim(fix): hardcode lualine theme
    nord theme does not support lualine and catppuccin colors for lualine
    are just better

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-lualine/lualine.nvim",
+	priority = 30,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
@@ -7,6 +8,33 @@ return {
 				icons_enabled = true,
 				component_separators = "|",
 				section_separators = "",
+				theme = {
+					normal = {
+						a = { fg = "#1e1e2e", bg = "#89b4fa" },
+						b = { fg = "#89b4fa", bg = "#313244" },
+						c = { bg = "NONE" },
+						x = { bg = "NONE" },
+					},
+					insert = {
+						a = { fg = "#1e1e2e", bg = "#a6e3a1" },
+						b = { fg = "a6e3a1", bg = "#313244" },
+						c = { bg = "NONE" },
+						x = { bg = "NONE" },
+					},
+					visual = {
+						a = { fg = "#1e1e2e", bg = "#cba6f7" },
+						b = { fg = "#cba6f7", bg = "#313244" },
+						c = { bg = "NONE" },
+						x = { bg = "NONE" },
+					},
+					command = {
+						a = { fg = "#1e1e2e", bg = "#fab387" },
+						b = { fg = "#fab387", bg = "#313244" },
+						c = { bg = "NONE" },
+						x = { bg = "NONE" },
+					},
+					-- leave everything else to the theme defaults
+				},
 			},
 			sections = {
 				lualine_a = { "filename" },
