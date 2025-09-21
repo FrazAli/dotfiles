b7966d1078f25df74bb696b621a71c90f5191f9a
Fraz Ali <frazaliqureshi@gmail.com>
2025-09-21 23:45:32 +0200

    feat(nvim): improved markdown preview icons

--- nvim/lua/plugins/formatting/render-markdown.lua
+++ nvim/lua/plugins/formatting/render-markdown.lua
@@ -3,7 +3,16 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	-- slanted bars	" ",
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		heading = {
+			width = "block",
+			position = "inline",
+			-- signs = { "① ", "② ", "③ ", "④ ", "⑤ ", "⑥ " },
+			signs = { " █", " █", " █", " █", " █", " █" },
+			icons = { "⓵  ", "⓶  ", "⓷  ", "⓸  ", "⓹  ", "⓺  " },
+		},
+	},
 }
