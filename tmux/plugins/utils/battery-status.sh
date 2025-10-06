4a5fae96a4f5d7cb6929b48f5351143c10cb7d60
Fraz Ali <frazaliqureshi@gmail.com>
2025-10-06 19:36:12 +0200

    feat(tmux): improved battery status

--- tmux/plugins/utils/battery-status.sh
+++ tmux/plugins/utils/battery-status.sh
@@ -13,17 +13,52 @@ case $batt_percent in
         ;;
 esac
 
-plugged="⚡️"
+is_discharging=false
+if echo "$batt_info" | grep -qi "discharging"; then
+    is_discharging=true
+fi
+
+batt_icon=""
+if (( batt_value >= 0 )); then
+    if $is_discharging; then
+        if (( batt_value == 100 )); then
+            batt_icon="󰁹"
+        elif (( batt_value >= 70 )); then
+            batt_icon="󱊣"
+        elif (( batt_value >= 30 )); then
+            batt_icon="󱊢"
+        elif (( batt_value >= 6 )); then
+            batt_icon="󱊡"
+        else # 0-5
+            batt_icon="󰂎"
+        fi
+    else # charging or fully charged
+        if (( batt_value == 100 )); then
+            batt_icon="󰂄"
+        elif (( batt_value >= 70 )); then
+            batt_icon="󱊦"
+        elif (( batt_value >= 30 )); then
+            batt_icon="󱊥"
+        elif (( batt_value >= 6 )); then
+            batt_icon="󱊤"
+        else # 0-5
+            batt_icon="󰢟"
+        fi
+    fi
+fi
+
 warning=""
 color_prefix=""
-if echo "$batt_info" | grep -qi "discharging"; then
-    plugged=""
+if $is_discharging; then
     (( batt_value >= 0 && batt_value <= 5 )) && { warning="🔴⚠️"; color_prefix="#[fg=#ff0000]"; }
     (( batt_value > 5 && batt_value <= 10 )) && { warning="⚠️"; color_prefix="#[fg=orange]"; }
 fi
+
+if (( batt_value >= 0 )); then
+    batt_status="${batt_icon} ${batt_percent}%"
+else
+    batt_status=""
+fi
+
 color_reset=${color_prefix:+#[fg=default]}
-
-batt_status="🔋${batt_percent}%"
-(( batt_value >= 0 && batt_value <= 20 )) && batt_status="🪫${batt_percent}%"
-
-echo "${color_prefix}${warning}${plugged}${batt_status}${color_reset}"
+echo "${color_prefix}${warning}${batt_status}${color_reset}"
