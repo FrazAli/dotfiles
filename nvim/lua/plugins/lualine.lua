8292698323f1e7d9dc2846a38bc517778b91ae6b
Fraz Ali <frazaliqureshi@gmail.com>
2025-10-06 21:37:56 +0200

    chore(nvim): adjusted lualine colors

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,11 +3,27 @@ return {
 	priority = 30,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		-- local colors = {
+		-- 	blue = "#61afef",
+		-- 	green = "#98c379",
+		-- 	purple = "#c678dd",
+		-- 	cyan = "#56b6c2",
+		-- 	red1 = "#e06c75",
+		-- 	red2 = "#be5046",
+		-- 	yellow = "#e5c07b",
+		-- 	fg = "#abb2bf",
+		-- 	bg = "#282c34",
+		-- 	gray1 = "#828997",
+		-- 	gray2 = "#2c323c",
+		-- 	gray3 = "#3e4452",
+		-- }
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
 				component_separators = "|",
 				section_separators = "",
+				-- catppuccin colors: #89b4fa, #a6e3a1, #cba6f7, #fab387
+				-- nord colors: #81a1c1, #98c379, #c678dd, #e5c07b
 				theme = {
 					normal = {
 						a = { fg = "#1e1e2e", bg = "#89b4fa" },
@@ -16,8 +32,8 @@ return {
 						x = { bg = "NONE" },
 					},
 					insert = {
-						a = { fg = "#1e1e2e", bg = "#a6e3a1" },
-						b = { fg = "a6e3a1", bg = "#313244" },
+						a = { fg = "#1e1e2e", bg = "#abb2bf" },
+						b = { fg = "#abb2bf", bg = "#313244" },
 						c = { bg = "NONE" },
 						x = { bg = "NONE" },
 					},
@@ -28,8 +44,8 @@ return {
 						x = { bg = "NONE" },
 					},
 					command = {
-						a = { fg = "#1e1e2e", bg = "#fab387" },
-						b = { fg = "#fab387", bg = "#313244" },
+						a = { fg = "#1e1e2e", bg = "#e5c07b" },
+						b = { fg = "#e5c07b", bg = "#313244" },
 						c = { bg = "NONE" },
 						x = { bg = "NONE" },
 					},
@@ -53,8 +69,8 @@ return {
 					},
 				},
 				lualine_x = {},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_y = { "location" },
+				lualine_z = { "progress" },
 			},
 		})
 	end,
